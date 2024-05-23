58d8f99b15080b7fab0acf70dfd66d6ed38a2a1b
depadavid <100303655+depadavid@users.noreply.github.com>
2024-05-23 15:14:37 -0500

    feat: :construction: Creación funcion filtrar por año
    Permite filtra la pelicula por año

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 fetch('https://search.imdbot.workers.dev/?q=action')
+.then(response => response.json())
+.then(data => {
+  console.log(filtrarPorAno(data, 2024));
+  console.log(filtrarPorActor(data, "Keanu Reeves"));
+  console.log(filtrarPorRangoIMDb(data, 1000, 5000));
+  console.log(obtenerTitulos(data));
+  console.log(obtenerTitulosYAnos(data));
+  console.log(obtenerIdentificadoresYTítulos(data));
+  console.log(obtenerURLsYTipos(data));
+  console.log(obtenerDetallesPeliculas(data));
+})
+.catch(error => console.error('Error:', error));
+
+function filtrarPorAno(data, ano) {
+  return data.filter(pelicula => pelicula["#YEAR"] === ano);
+}
+
